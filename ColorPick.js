716e9176cba49cf97db26d0a16655b1c3b7cd839
Phil Zet <Phil@MacBook-Pro-Filipp.local>
2017-01-21 16:24:30 +0300

    Removed z-index from the defaults

--- ColorPick.js
+++ ColorPick.js
@@ -21,7 +21,6 @@
     
     $.fn.colorPick.defaults = {
         'initialColor': '#3498db',
-        'z-index': '1000',
         'allowRecent': true,
         'recentMax': 5,
         'palette': ["#1abc9c", "#16a085", "#2ecc71", "#27ae60", "#3498db", "#2980b9", "#9b59b6", "#8e44ad", "#34495e", "#2c3e50", "#f1c40f", "#f39c12", "#e67e22", "#d35400", "#e74c3c", "#c0392b", "#ecf0f1", "#bdc3c7", "#95a5a6", "#7f8c8d"],
@@ -81,7 +80,7 @@
         },
         
         show: function(left, top) {
-	        $("body").append('<div id="colorPick" style="display:none;top:' + top + 'px;left:' + left + 'px;"><span>Default palette:</span></div>');
+	        $("body").append('<div id="colorPick" style="display:none;top:' + top + 'px;left:' + left + 'px"><span>Default palette:</span></div>');
 	        jQuery.each($.fn.colorPick.defaults.palette, (index, item) => {
 		        $("#colorPick").append('<div class="colorPickButton" hexValue="' + item + '" style="background:' + item + '"></div>');
 			});
